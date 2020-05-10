535c3ea8594f3ccf15f3893107b3f45f4ad2e782
ChabaJosa <chabagjg@gmail.com>
2020-05-09 21:39:03 -0400

    Got basic frontend app working

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -1,15 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+import {GlobalContext} from "../context/GlobalState"
+
 
 export const AddTransaction = () => {
 
     const [text, setText] = useState("")
     const [amount, setAmount] = useState(0)
 
+    const {addTransaction}   = useContext(GlobalContext)
+
+    const addThis = (e) => {
+        e.preventDefault()
+        const newTransaction = {
+            id: Math.floor(Math.random() * 1000000000),
+            text,
+            amount
+        }
+
+        addTransaction(newTransaction)
+    }
+
 
     return (
         <>
             <h3>Add new transaction</h3>
-            <form id="form">
+            <form id="form" onSubmit={addThis}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
@@ -19,7 +34,7 @@ export const AddTransaction = () => {
                     >Amount <br />
                 (negative - expense, positive - income)</label
                     >
-                    <input type="number" value={text} onChange={(e) => setText(e.target.value)} id="amount" placeholder="Enter amount..." />
+                    <input type="number" value={amount} onChange={(e) => setAmount(Number(e.target.value))} id="amount" placeholder="Enter amount..." />
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
