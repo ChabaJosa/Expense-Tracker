9b4c627b0ffc622298012c87c5dc8e76733131b6
ChabaJosa <chabagjg@gmail.com>
2020-05-10 21:48:51 -0400

    Updates

---
+++ precache-manifest.a6f57b3b4b3cf37e7b7c34b272f44c05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99fb976c1cf324fa7e09c27b08b65e2",
+    "url": "/Expense-Tracker/index.html"
+  },
+  {
+    "revision": "22b408042291fca8dc72",
+    "url": "/Expense-Tracker/static/css/main.79b675ef.chunk.css"
+  },
+  {
+    "revision": "f35695db11317516d110",
+    "url": "/Expense-Tracker/static/js/2.c3734ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker/static/js/2.c3734ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b408042291fca8dc72",
+    "url": "/Expense-Tracker/static/js/main.95c0f58b.chunk.js"
+  },
+  {
+    "revision": "df36d442ca549fef3446",
+    "url": "/Expense-Tracker/static/js/runtime-main.01781d51.js"
+  }
+]);
