50236a55756dc526baaafa3798f52b0481556d06
ChabaJosa <chabagjg@gmail.com>
2020-05-16 18:12:06 -0400

    Reverted back to previous commit deu to deployment issues but added mobile responsiveness

--- src/components/ExpenseTracker.jsx
+++ src/components/ExpenseTracker.jsx
@@ -1,18 +1,52 @@
-import React from 'react';
-import { GlobalProvider } from "../context/GlobalState"
-import { Header } from "./ExpenseTracker/Expenses/Header"            // Curly braces are required here because of the ES7 syntax, 
-import { Balance } from "./ExpenseTracker/Expenses/Balance"           // not sure if it's desctrocturing
-import { IncomeExpenses } from "./ExpenseTracker/Expenses/IncomeExpenses"
-import { TransactionList } from "./ExpenseTracker/Expenses/TransactionList"
-import { AddTransaction } from "./ExpenseTracker/Expenses/AddTransaction"
-// import { Container } from "react-bootstrap";
+import React                    from 'react';
+import { GlobalProvider }       from "../context/GlobalState"
+import { Header }               from "./ExpenseTracker/Expenses/Header"            // Curly braces are required here because of the ES7 syntax, 
+import { Balance }              from "./ExpenseTracker/Expenses/Balance"           // not sure if it's desctrocturing
+import { IncomeExpenses }       from "./ExpenseTracker/Expenses/IncomeExpenses"
+import { TransactionList }      from "./ExpenseTracker/Expenses/TransactionList"
+import { AddTransaction }       from "./ExpenseTracker/Expenses/AddTransaction"
+// import { Container }         from "react-bootstrap";
+import Pic1                     from "./ExpenseTracker/Expenses/backgroundImages/dollar.png"
+import Pic2                     from "./ExpenseTracker/Expenses/backgroundImages/calculator.jpg"
+import Pic3                     from "./ExpenseTracker/Expenses/backgroundImages/entrepreneur.jpg"
+import Pic4                     from "./ExpenseTracker/Expenses/backgroundImages/piggy-bank.jpg"
 
 
 const ExpenseTracker = (props) => {
     console.log(props)
+
+    const backImgs = [Pic1, Pic2, Pic3, Pic4]
+
+    const randomElement = (itemsArray) => { 
+        let randomItem = Math.floor(Math.random() * itemsArray.length); 
+        console.log(randomItem); 
+        return itemsArray[randomItem]
+    } 
+
+
     return (
         <GlobalProvider>
             <>
+            <div className="d-flex flex-row justify-content-around" style={{
+                backgroundSize:"cover", 
+                backgroundRepeat:" no-repeat",
+                backgroundImage:  `linear-gradient(
+                    rgba(0, 0, 0, 0.5),
+                    rgba(0, 0, 0, 0.5)
+                    ),url(${randomElement(backImgs)})`
+                
+            }}
+            >
+                <div>
+                    <Header />
+                    <>
+                        <Balance />
+                        <IncomeExpenses />
+                        <TransactionList />
+                        <AddTransaction />
+                    </>
+
+                </div>
 
                 <div>
                     <Header />
@@ -23,9 +57,19 @@ const ExpenseTracker = (props) => {
                         <AddTransaction />
                     </>
 
-
                 </div>
 
+                <div>
+                    <Header />
+                    <>
+                        <Balance />
+                        <IncomeExpenses />
+                        <TransactionList />
+                        <AddTransaction />
+                    </>
+
+                </div>
+            </div>
 
             </>
         </GlobalProvider>
