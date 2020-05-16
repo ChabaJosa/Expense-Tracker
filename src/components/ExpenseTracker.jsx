34b20cb42b9d33c29610cf235bcf5cd9f6b4ce77
ChabaJosa <chabagjg@gmail.com>
2020-05-15 23:39:32 -0400

    Re-organazing the folders

--- src/components/ExpenseTracker.jsx
+++ src/components/ExpenseTracker.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { GlobalProvider } from "../context/GlobalState"
-import { Header } from "./ExpenseTracker/Header"            // Curly braces are required here because of the ES7 syntax, 
-import { Balance } from "./ExpenseTracker/Balance"           // not sure if it's desctrocturing
-import { IncomeExpenses } from "./ExpenseTracker/IncomeExpenses"
-import { TransactionList } from "./ExpenseTracker/TransactionList"
-import { AddTransaction } from "./ExpenseTracker/AddTransaction"
+import { Header } from "./ExpenseTracker/Expenses/Header"            // Curly braces are required here because of the ES7 syntax, 
+import { Balance } from "./ExpenseTracker/Expenses/Balance"           // not sure if it's desctrocturing
+import { IncomeExpenses } from "./ExpenseTracker/Expenses/IncomeExpenses"
+import { TransactionList } from "./ExpenseTracker/Expenses/TransactionList"
+import { AddTransaction } from "./ExpenseTracker/Expenses/AddTransaction"
 // import { Container } from "react-bootstrap";
 
 
