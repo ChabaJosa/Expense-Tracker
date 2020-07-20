ef01ffc596e7af941ef6b1461b69334cfd0bac80
ChabaJosa <chabagjg@gmail.com>
2020-07-19 21:57:30 -0400

    Deployment not working, fixed one error removing the white listIP Resctricion from Mongo, currently making a small change to the server.js to see if it works

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/v1/transactions', transactions)
 
 // This must be below the api routes
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'))
+    app.use(express.static('client/build'))
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
 }
 
