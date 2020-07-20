07de407286b106365297a54da1136ccce1ad326c
ChabaJosa <chabagjg@gmail.com>
2020-07-19 22:08:51 -0400

    Trying other changes to the server.js file to see if it picks up the index.html in the build folder

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
-const path = require('path')
-const express = require('express')
-const dotenv = require('dotenv')
-const colors = require('colors')
-const morgan = require('morgan')
-const connectDB = require('./config/db')
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
-dotenv.config({path:"./config/config.env"})
+dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
-const transactions = require('./routes/transactions')
-const app = express()
+const transactions = require("./routes/transactions");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use('/api/v1/transactions', transactions)
+app.use("/api/v1/transactions", transactions);
 
 // This must be below the api routes
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    const index = path.join(__dirname, "build", "index.html");
+    res.sendFile(index);
+  });
 }
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow));
 
-
-
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+  )
+);
