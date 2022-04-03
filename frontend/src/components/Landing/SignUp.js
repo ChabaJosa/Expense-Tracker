98a0477e470fe67a05172f27103f54bfba0ded4d
ChabaJosa <chabagjg@gmail.com>
2022-04-03 10:25:27 -0400

    Added dotenv require

--- frontend/src/components/Landing/SignUp.js
+++ frontend/src/components/Landing/SignUp.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function SignUp() {
-    return (
-        <div style={{ color: "#fff", padding:'8px',marigin:'16px'  }}>
-            Hello from Sign Up!
-        </div>
-    )
+  return (
+    <div style={{ color: "#fff", padding: "8px", marigin: "16px" }}>
+      Hello from Sign Up!
+    </div>
+  );
 }
